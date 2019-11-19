2d69c4fa1f6ae310f8f1cbaaf8ff8cd4ff7b7f79
CORTC068 <Cristian.X.Cortes.-ND@disney.com>
2019-11-18 21:45:26 -0500

    scaffolding ap

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-import MovementsList from './containers/movements/movementsList.js';
+import Home from './containers/home.js';
+import UserProfile from './containers/userProfile.js';
 
 export default class App extends Component {
+
+    constructor(props){
+		super(props);
+		
+		this.state = {
+			currentSection: 'home'
+		}
+	}
+
+    showUserProfile(item) {
+        this.setState({ currentSection: 'userProfile', userName: item.name })
+    }
+
     render(){
         return(
             <div className="container">
                 <div className="row">
-                    <div className="col-md-12">
-                        <h1 className="main-title">Debits / Credits App</h1>
-                        <hr/>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-md-6">
-                        <MovementsList type={'debit'}/>
+                    <div className="col-md-3">
                     </div>
                     <div className="col-md-6">
-                        <MovementsList type={'credit'}/>
+                        { this.state.currentSection == 'home' ? <Home showUserProfile={ (item) => this.showUserProfile(item) }/> : <UserProfile userName={ this.state.userName } /> }
+                    </div>
+                    <div className="col-md-3">
                     </div>
                 </div>
             </div>
